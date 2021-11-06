85cf19e13551b4c80ce352d26d60b8e362c3b2b5
Dvent1123 <ventura.daniel36@gmail.com>
2021-11-06 10:24:34 -0700

    command folder structured

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 require('dotenv').config()
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { Client, Intents, Collection } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, 
+    Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+
+const prefix = '-'
+
+const fs = require('fs')
+
+client.commands = new Collection()
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+
+    client.commands.set(command.name, command)
+}
 
 client.once('ready', () => {
     console.log('Daddy Bezos is online')
@@ -8,6 +23,18 @@ client.once('ready', () => {
 
 
 
+client.on('messageCreate' , message => {
+    //if not prefixed or is bot then return
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    //splits message after prefix
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args)
+    }
+})
 
 
 client.login(`${process.env.TOKEN}`)
